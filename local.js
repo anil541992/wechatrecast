86e5940fa3541363bd41c17c25fd4ed146633c0a
anil541992 <33116355+anil541992@users.noreply.github.com>
2018-12-18 21:38:27 +0530

    Update local.js

--- local.js
+++ local.js
@@ -1,8 +1,8 @@
 var path = require('path'), express = require('express');
 var qs = require('querystring');
 var app = express();
-var routesEngine = require('./index.js'); 
-routesEngine(app);
+//var routesEngine = require('./index.js'); 
+//routesEngine(app);
 
 //var request = require('request');
 //var jsSHA = require('jssha');
